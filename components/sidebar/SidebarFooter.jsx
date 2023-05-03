e4a3cedd2f8f0507d4717562f8f05a883ca650fd
oscareimart <115655099+oscareimart@users.noreply.github.com>
2023-05-03 11:05:37 -0400

    Update SidebarFooter.jsx

--- components/sidebar/SidebarFooter.jsx
+++ components/sidebar/SidebarFooter.jsx
@@ -3,18 +3,36 @@ import React from 'react'
 import styles from './../../scss/style.module.scss'
 
 const SidebarFooter = (props) => {
+    const { dataCompany } = props
     return (
         <>
             <div className={styles.menuFooter}>
-                <Link href="https://www.instagram.com/visionmoderna_vm/">
-                    <i className="fa-brands fa-instagram"></i>
-                </Link>
-                <Link href="https://web.whatsapp.com/">
+                {
+                    dataCompany?.state?.social_networks?.data?.map((row, i) => (
+                        <Link
+                            key={i}
+                            href={row.attributes?.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className={row.attributes?.icon}></i>
+                        </Link>
+                    ))
+                }
+                {/* <Link
+                    href="https://web.whatsapp.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <i className="fa-brands fa-whatsapp"></i>
                 </Link>
-                <Link href="https://www.facebook.com/visionmoderna">
+                <Link
+                    href="https://www.facebook.com/visionmoderna"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <i className="fa-brands fa-facebook-f"></i>
-                </Link>
+                </Link> */}
             </div>
         </>
     )
