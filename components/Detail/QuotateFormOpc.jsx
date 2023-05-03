58f502ae8004e3b3cbecd180339be7d8fbe81aad
oscareimart <115655099+oscareimart@users.noreply.github.com>
2023-05-03 11:02:08 -0400

    Update QuotateFormOpc.jsx

--- components/Detail/QuotateFormOpc.jsx
+++ components/Detail/QuotateFormOpc.jsx
@@ -3,7 +3,7 @@ import styles from './../../scss/style.module.scss'
 
 const QuoteFormOpc = (props) => {
     const { dataQuotate, showQuote, dataOpt, optCheckData = [] } = props
-    console.log(dataOpt)
+    // console.log(dataOpt)
     // useEffect(() => {
     //     if (dataOpt.attributes?.detail_interest?.length > 0) {
     //         let arrayTemp = []
@@ -55,73 +55,73 @@ const QuoteFormOpc = (props) => {
                         />
                     </div>
                 </div>
-                <form autoComplete='off'>
-                    <div className="row mx-3 mt-2">
-                        <div className="col-md-12 mb-3">
-                            <label className='col-form-label'>Nombre:</label>
-                            <input
-                                className={`form-control ${styles.inputCustom}`}
-                                type="text"
-                                name='name'
-                                autoComplete="new-name"
-                                placeholder='Nombre'
-                                onChange={(e) => onChange(e.target)}
-                                value={dataQuotate?.state?.name}
-                            />
-                        </div>
-                        <div className="col-md-12 mb-3">
-                            <label className='col-form-label'>Telefono:</label>
-                            <input
-                                className={`form-control ${styles.inputCustom}`}
-                                type="text"
-                                name='phone'
-                                autoComplete="new-phone"
-                                placeholder='Telefono'
-                                onChange={(e) => onChange(e.target)}
-                                value={dataQuotate?.state?.phone}
-                            />
-                        </div>
-                        <div className="col-md-12 mb-3">
-                            <label className='col-form-label'>Correo Electrónico:</label>
-                            <input
-                                className={`form-control ${styles.inputCustom}`}
-                                type="email"
-                                name='email'
-                                autoComplete="new-email"
-                                placeholder='Correo Electrónico'
-                                onChange={(e) => onChange(e.target)}
-                                value={dataQuotate?.state?.email}
-                            />
-                        </div>
-                        <div className="col-md-12 mb-3">
-                            <label className='col-form-label mb-1'>Departamento de Interes:</label>
-
-                            {
-                                dataOpt.attributes?.departments?.data?.length > 0 ? <>
-                                    {
-                                        dataOpt.attributes?.departments?.data?.map((row, i) => (
-                                            <div className="form-check ms-2" key={i}>
-                                                <input
-                                                    className={`form-check-input ${styles.checkCustom}`}
-                                                    type="checkbox"
-                                                    id={`opt${i}`}
-                                                    onChange={() => onChangeCheck(row, i)}
-                                                    value={row.attributes?.code}
-                                                />
-                                                <label className="form-check-label" htmlFor={`opt${i}`}>
-                                                    {/* {row.name}{row.description && ` | ${row.description}`} */}
-                                                    {row.attributes?.title}
-                                                </label>
-                                            </div>
-                                        ))
-                                    }
-                                </> : <div>
-                                    <label className=''>Sin Opciones</label>
-                                </div>
-                            }
-                        </div>
+                <div className="row mx-3 mt-2">
+                    <div className="col-md-12 mb-3">
+                        <label className='col-form-label'>Nombre:</label>
+                        <input
+                            className={`form-control ${styles.inputCustom}`}
+                            type="text"
+                            name='name'
+                            autoComplete="new-name"
+                            placeholder='Nombre'
+                            onChange={(e) => onChange(e.target)}
+                            value={dataQuotate?.state?.name}
+                            required
+                        />
                     </div>
-                </form>
+                    <div className="col-md-12 mb-3">
+                        <label className='col-form-label'>Telefono:</label>
+                        <input
+                            className={`form-control ${styles.inputCustom}`}
+                            type="text"
+                            name='phone'
+                            autoComplete="new-phone"
+                            placeholder='Telefono'
+                            onChange={(e) => onChange(e.target)}
+                            value={dataQuotate?.state?.phone}
+                        />
+                    </div>
+                    <div className="col-md-12 mb-3">
+                        <label className='col-form-label'>Correo Electrónico:</label>
+                        <input
+                            className={`form-control ${styles.inputCustom}`}
+                            type="email"
+                            name='email'
+                            autoComplete="new-email"
+                            placeholder='Correo Electrónico'
+                            onChange={(e) => onChange(e.target)}
+                            value={dataQuotate?.state?.email}
+                            required
+                        />
+                    </div>
+                    <div className="col-md-12 mb-3">
+                        <label className='col-form-label mb-1'>Departamento de Interes:</label>
+
+                        {
+                            dataOpt.attributes?.departments?.data?.length > 0 ? <>
+                                {
+                                    dataOpt.attributes?.departments?.data?.map((row, i) => (
+                                        <div className="form-check ms-2" key={i}>
+                                            <input
+                                                className={`form-check-input ${styles.checkCustom}`}
+                                                type="checkbox"
+                                                id={`opt${i}`}
+                                                onChange={() => onChangeCheck(row, i)}
+                                                value={row.attributes?.code}
+                                            />
+                                            <label className="form-check-label" htmlFor={`opt${i}`}>
+                                                {/* {row.name}{row.description && ` | ${row.description}`} */}
+                                                {row.attributes?.title}
+                                            </label>
+                                        </div>
+                                    ))
+                                }
+                            </> : <div>
+                                <label className=''>Sin Opciones</label>
+                            </div>
+                        }
+                    </div>
+                </div>
             </div >
         </>
     )
